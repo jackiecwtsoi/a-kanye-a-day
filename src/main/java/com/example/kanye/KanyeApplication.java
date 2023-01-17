6bae758b1fccf7b2002a1964796a1ff52734d3e8
Jackie Tsoi <jackiecwtsoi@gmail.com>
2023-01-17 19:28:20 +0800

    Initial commit.

---
+++ src/main/java/com/example/kanye/KanyeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanyeApplication.class, args);
+	}
+
+}
